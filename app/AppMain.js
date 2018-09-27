745c992dc828a9e2c8193302998aaabb57edecf7
ken <ken.lee@aurorainno.com>
2018-09-28 00:15:05 +0800

    Add some test work with the state

--- app/AppMain.js
+++ app/AppMain.js
@@ -1,17 +1,75 @@
 import React, {Component} from 'react';
 //import Home from './components/Home/Home.component';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Button, Text, View} from 'react-native';
+
+import {TEST_ACTION} from './redux/actions/index.actions';
 
 import {connect} from 'react-redux';
 
 class AppMain extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "counter" : 0,
+      "mainState" : JSON.stringify(this.props.state)
+    };
+  }
+
+  onPress1 = () => {
+    console.log("Button is pressed. counter=" + this.state.counter);
+
+    let newCounter = this.state.counter + 1;
+    //this.setState( { "counter" : newCounter });
+    this.props.state.test = {"test" : newCounter};
+
+    this.state.mainState = JSON.stringify(this.props.state);
+    this.state.counter = newCounter;
+    
+    this.setState({});  // notify that the state are updated
+    
+    console.log("core state" + JSON.stringify(this.props.state));
+  }
+
+  onPress2 = () => {
+    console.log("Button is pressed. counter=" + this.state.counter);
+
+    let newCounter = this.state.counter - 1;
+    //this.setState( { "counter" : newCounter });
+    this.props.state.test = {"test" : newCounter};
+    this.props.state.test2 = {"counter" : newCounter};
+
+
+
+    this.setState({
+      "mainState" : JSON.stringify(this.props.state),
+      "counter" : newCounter
+    });
+    
+    console.log("core state" + JSON.stringify(this.props.state));
+  }
+
+  onPressTestAction = () => {
+    console.log("onPressTestAction is pressed");
+    //this.props.dispatch()
+  
+    dispatch(TEST_ACTION);
+  }
+
   render () {
     console.log(this.props.state); // eslint-disable-line
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
         <Text>
-            { JSON.stringify(this.props.state) }
+            { this.state.mainState }
         </Text>
+        <Text>
+            { this.state.counter }
+        </Text>
+        <Button title="Test+1" onPress={this.onPress1} />
+        <Button title="Test-1" onPress={this.onPress2} />
+        <Button title="TestAction" onPress={this.onPressTestAction} />
+      
+
       </View>
     );
   }
--- app/redux/reducers/rootReducer.js
+++ app/redux/reducers/rootReducer.js
@@ -3,6 +3,6 @@ import test from './test.reducer';
 import test2 from './test.reducer';
 
 export default combineReducers({
-    test,
-    test2
+    test,                // First state named 'test' e.g this.props.state.test
+    test2               // Second State named 'test2' e.g this.props.state.test2
 });
